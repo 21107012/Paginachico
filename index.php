7c3efe414246b250cb5ebb1e80ace7ff8c83add2
21107012 <21107012@utfv.edu.mx>
2023-03-31 12:14:55 -0600

    repocitorio

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <title>TUVSA</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+
+            <!-- MENU -->
+
+<?php
+
+include "assets/inc/header.inc";
+
+?>
+
+<h1>Inicio</h1>
+            <!-- MENU -->
+
+
+            <?php
+
+include "assets/inc/footer.inc";
+
+?>
+
+
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
